6003e0f4d634ac3c197e6a1aba79fa0c1017fd0c
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2026-07-22 15:48:01 +0200

    stashing commit

---
+++ launch/ros2/cbf_pc_selector_robot.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Declare the config file argument
+    declare_cfg_arg = DeclareLaunchArgument(
+        'cfg',
+        default_value='cbf_pc_selector_robot.yaml',
+        description='Config file name (only filename, not full path)'
+    )
+    
+    # Create the full path to the config file
+    cfg_file = PathJoinSubstitution([
+        get_package_share_directory('robot_bringup'),
+        'config',
+        'ros2',
+        LaunchConfiguration('cfg')
+    ])
+    
+    node = Node(
+        package='cbf_pc_selector',
+        executable='pc_selector_node',
+        name='cbf_pc_selector',
+        parameters=[
+            cfg_file,
+        ],
+        output='screen'
+    )
+    
+    return LaunchDescription([
+        declare_cfg_arg,
+        node
+    ])
---
+++ launch/ros2/nmpc_full_robot.launch.py
@@ -0,0 +1,139 @@
+from launch import LaunchDescription
+from launch.actions import GroupAction, DeclareLaunchArgument
+from launch_ros.actions import Node, PushRosNamespace, SetParameter, SetRemap
+from launch.substitutions import LaunchConfiguration, PythonExpression, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    ns = LaunchConfiguration('ns')
+    # cfg = LaunchConfiguration('cfg')
+    # build = LaunchConfiguration('build')
+
+    declare_args = [
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
+        DeclareLaunchArgument('ns', default_value='sdf_nmpc', description='Common namespace for nmpc nodes'),
+        DeclareLaunchArgument(
+            'input_image', 
+            default_value='/img_node/range_image',
+            description='Topic to remap to observation'
+        ),
+        DeclareLaunchArgument(
+            'odometry', 
+            default_value='/msf_core/odometry',
+            description='Odometry topic'
+        ),
+        DeclareLaunchArgument(
+            'cfg', 
+            default_value='nmpc_robot.yaml',
+            description='Config file'
+        ),
+        # DeclareLaunchArgument('cfg', default_value='sim_camera.yaml', description='Config preset <cfg>.yaml'),
+        # # DeclareLaunchArgument('build', default_value='false', description='Run pre-build step before starting nodes'),
+    ]
+
+    odometry = LaunchConfiguration('odometry')
+    input_image = LaunchConfiguration('input_image')
+    cfg = LaunchConfiguration('cfg')
+
+    cfg_file = PathJoinSubstitution([
+        get_package_share_directory('robot_bringup'),
+        'config',
+        'ros2',
+        cfg
+    ])
+
+    node_vae = Node(
+        package='sdf_nmpc_ros',
+        executable='vae_node.py',
+        name='vae',
+        parameters=[{
+            'cfg': cfg_file,
+            'use_sim_time': use_sim_time,
+        }],
+        output='screen'
+    )
+
+    node_ref_gen = Node(
+        package='sdf_nmpc_ros',
+        executable='ref_gen_node.py',
+        name='ref_gen',
+        parameters=[{
+            'cfg': cfg_file,
+            'use_sim_time': use_sim_time,
+        }],
+        output='screen'
+    )
+
+    node_sdfnmpc = Node(
+        package='sdf_nmpc_ros',
+        executable='sdfnmpc_node.py',
+        name='sdfnmpc',
+        parameters=[{
+            'cfg': cfg_file,
+            'use_sim_time': use_sim_time,
+        }],
+        output='screen'
+    )
+    
+
+    node_viz_vae = Node(
+        package='sdf_nmpc_ros',
+        executable='viz_vae_node.py',
+        name='viz_vae',
+        parameters=[{
+            'cfg': cfg_file,
+            'use_sim_time': use_sim_time,
+        }],
+        output='screen'
+    )
+
+    node_viz_sdf_2D = Node(
+        package='sdf_nmpc_ros',
+        executable='viz_sdf_2D_node.py',
+        name='viz_sdf_2D',
+        parameters=[{
+            'cfg': cfg_file,
+            'use_sim_time': use_sim_time
+        }],
+        output='screen'
+    )
+
+    node_viz_sdf_3D = Node(
+        package='sdf_nmpc_ros',
+        executable='viz_sdf_3D_node.py',
+        name='viz_sdf_3D',
+        parameters=[{
+            'cfg': cfg_file,
+            'use_sim_time': use_sim_time
+        }],
+        output='screen'
+    )
+
+    node_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(get_package_share_directory('sdf_nmpc_ros'), 'rviz', 'nmpc.rviz')]
+    )
+
+    group = GroupAction([
+        PushRosNamespace(ns),  # this namespace is expected by rviz_nmpc_plugin
+        SetRemap(src='odometry', dst=odometry),
+        # SetRemap(src='observation', dst='/rmf/cam/depth'),
+        # SetRemap(src='observation', dst='/rmf/lidar/range'),
+        SetRemap(src='observation', dst=input_image),
+        # SetRemap(src='cmd/acc', dst='/rmf/cmd/acc'),
+        SetRemap(src='cmd/acc', dst='/sdf_nmpc/cmd/acc'),
+        SetRemap(src='wps', dst='/gbplanner_path'),
+        node_vae,
+        node_ref_gen,
+        node_sdfnmpc,
+        # node_rviz,
+        # node_viz_vae,
+        # node_viz_sdf_2D,
+    ])
+    
+    return LaunchDescription(declare_args + [group])
